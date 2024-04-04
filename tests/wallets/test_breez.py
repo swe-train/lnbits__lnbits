75f04a390892431d6e83478db8f4211ad6d0aedc
Vlad Stan <stan.v.vlad@gmail.com>
2024-04-04 11:12:33 +0300

    feat: `test_create_invoice` draft

--- tests/wallets/test_breez.py
+++ tests/wallets/test_breez.py
@@ -1,3 +1,4 @@
+import breez_sdk
 import pytest
 from mock import Mock
 from pytest_mock.plugin import MockerFixture
@@ -24,7 +25,6 @@ async def test_status(mocker: MockerFixture):
     mocker.patch("breez_sdk.connect").return_value({})
     wallet = BreezSdkWallet()
 
-
     mocker.patch.object(
         wallet.sdk_services,
         "node_info",
@@ -32,8 +32,68 @@ async def test_status(mocker: MockerFixture):
     )
 
     status = await wallet.status()
-    print("### status", status)
+    print("### status 1", status)
+
+    m = Mock(side_effect=Exception("Boom!"))
+
+    mocker.patch.object(wallet.sdk_services, "node_info", m)
+    status = await wallet.status()
+    print("### status 2", status)
+
+
+@pytest.mark.asyncio
+async def test_create_invoice(mocker: MockerFixture):
+
+    settings.breez_api_key = "100"
+    settings.breez_greenlight_seed = (
+        "push divert icon era bracket fade much kind reason injury suffer muffin"
+    )
+    settings.breez_greenlight_device_key = "tests/wallets/data/certificates/cert.pem"
+    settings.breez_greenlight_device_cert = "tests/wallets/data/certificates/breez.crt"
+
+    mocker.patch("breez_sdk.connect").return_value({})
+    wallet = BreezSdkWallet()
+
+    mocker.patch.object(
+        wallet.sdk_services,
+        "receive_payment",
+        _data_mock({"ln_invoice": {"payment_hash": "000001", "bolt11": "ln1234"}}),
+    )
+
+    spy = mocker.spy(wallet.sdk_services, "receive_payment")
+    invoice_response = await wallet.create_invoice(50, "Test Invoice")
+    print("### invoice_response 1", invoice_response)
+
+    assert spy.call_count == 1
+
+    resp = breez_sdk.ReceivePaymentRequest(
+        amount_msat=50 * 1000,  # breez uses msat
+        description="Test Invoice",
+        preimage=None,
+        opening_fee_params=None,
+        use_description_hash=None,
+    )
+    spy.assert_called_with(resp)
+
+    print("### spy.call_args 1", spy.call_args)
+    print("### spy.call_args 2", getattr(spy.call_args,"amount_msat"))
+
+    # m = Mock(side_effect=Exception("Boom!"))
+
+    # mocker.patch.object(wallet.sdk_services, "receive_payment", m)
+    # invoice_response = await wallet.create_invoice(50, "Test Invoice")
+    # print("### invoice_response 2", invoice_response)
+
+
+def _dict_to_object(data: dict) -> DataObject:
+    d = {**data}
+    for k in data:
+        value = data[k]
+        if isinstance(value, dict):
+            d[k] = _dict_to_object(value)
+
+    return DataObject(**d)
 
 
 def _data_mock(data: dict) -> Mock:
-    return Mock(return_value=DataObject(**data))
+    return Mock(return_value=_dict_to_object(data))
