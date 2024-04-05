13818395c4b36e50e473a339dcc32d883df5cdbc
Vlad Stan <stan.v.vlad@gmail.com>
2024-04-05 13:46:54 +0300

    chore: fix the fix

--- lnbits/wallets/corelightning.py
+++ lnbits/wallets/corelightning.py
@@ -40,7 +40,7 @@ class CoreLightningWallet(Wallet):
 
         self.ln = LightningRpc(rpc)
         # check if description_hash is supported (from corelightning>=v0.11.0)
-        command = self.ln.help("invoice")x["help"][0]["command"]  # type: ignore
+        command = self.ln.help("invoice")["help"][0]["command"]  # type: ignore
         self.supports_description_hash = "deschashonly" in command
 
         # check last payindex so we can listen from that point on
